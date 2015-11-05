c90f8b5e6a6e20ee89812081c8254b6fa81c5f04
Jongmin Kim <kjm141215@gmail.com>
2015-11-05 16:18:40 +0000

    Initial commit: sending an audio file via bluetooth

---
+++ CBTutorial/CBCentralManagerViewController.m
@@ -0,0 +1,193 @@
+//
+//  CBCentralManagerViewController.m
+//  CBTutorial
+//
+//  Created by Orlando Pereira on 10/8/13.
+//  Copyright (c) 2013 Mobiletuts. All rights reserved.
+//
+
+#import "CBCentralManagerViewController.h"
+
+@implementation CBCentralManagerViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+    _data = [[NSMutableData alloc] init];
+    
+}
+
+- (void) setupAudio {
+    NSError * error;
+    [[AVAudioSession sharedInstance] setActive:YES error:&error];
+    if (error != nil) {
+        NSAssert(error == nil, @"");
+    }
+    
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+    if (error != nil) {
+        NSAssert(error == nil, @"");
+    }
+    self.audioPlayer = [[AVAudioPlayer alloc] initWithData:self.data error:&error];
+    if (error != nil) {
+        NSAssert(error == nil, @"");
+    }
+    
+    [self.audioPlayer setVolume:0.8];
+    [self.audioPlayer prepareToPlay];
+
+}
+- (IBAction)playButtonPressed:(id)sender {
+    BOOL played = [self.audioPlayer play];
+    if (!played) {
+        NSLog(@"Error");
+    }
+}
+- (IBAction)stopButtonPressed:(id)sender {
+    [self.audioPlayer stop];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [_centralManager stopScan];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
+    NSLog(@"Discovered %@ at %@", peripheral.name, RSSI);
+    
+    if (_discoveredPeripheral != peripheral) {
+        // Save a local copy of the peripheral, so CoreBluetooth doesn't get rid of it
+        _discoveredPeripheral = peripheral;
+        
+        // And connect
+        NSLog(@"Connecting to peripheral %@", peripheral);
+        [_centralManager connectPeripheral:peripheral options:nil];
+    }
+}
+
+- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
+    NSLog(@"Failed to connect");
+    [self cleanup];
+}
+
+- (void)cleanup {
+    
+    // See if we are subscribed to a characteristic on the peripheral
+    if (_discoveredPeripheral.services != nil) {
+        for (CBService *service in _discoveredPeripheral.services) {
+            if (service.characteristics != nil) {
+                for (CBCharacteristic *characteristic in service.characteristics) {
+                    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]]) {
+                        if (characteristic.isNotifying) {
+                            [_discoveredPeripheral setNotifyValue:NO forCharacteristic:characteristic];
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    [_centralManager cancelPeripheralConnection:_discoveredPeripheral];
+}
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    // You should test all scenarios
+    if (central.state != CBCentralManagerStatePoweredOn) {
+        return;
+    }
+    
+    if (central.state == CBCentralManagerStatePoweredOn) {
+        // Scan for devices
+        [_centralManager scanForPeripheralsWithServices:@[[CBUUID UUIDWithString:TRANSFER_SERVICE_UUID]] options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
+        NSLog(@"Scanning started");
+    }
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
+    NSLog(@"Connected");
+    
+    [_centralManager stopScan];
+    NSLog(@"Scanning stopped");
+    
+    [_data setLength:0];
+    
+    peripheral.delegate = self;
+    
+    [peripheral discoverServices:@[[CBUUID UUIDWithString:TRANSFER_SERVICE_UUID]]];
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
+    if (error) {
+        [self cleanup];
+        return;
+    }
+    
+    for (CBService *service in peripheral.services) {
+        [peripheral discoverCharacteristics:@[[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]] forService:service];
+    }
+    // Discover other characteristics
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error {
+    if (error) {
+        [self cleanup];
+        return;
+    }
+    
+    for (CBCharacteristic *characteristic in service.characteristics) {
+        if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]]) {
+            [peripheral setNotifyValue:YES forCharacteristic:characteristic];
+        }
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
+    if (error) {
+        NSLog(@"Error");
+        return;
+    }
+    
+    NSString *stringFromData = [[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+    NSLog(@"data %i", self.data.length);
+    // Have we got everything we need?
+    if ([stringFromData isEqualToString:@"EOM"]) {
+        NSLog(@"data %i", self.data.length);
+        [self setupAudio];
+        [_textview setText:[[NSString alloc] initWithData:self.data encoding:NSUTF8StringEncoding]];
+        
+        [peripheral setNotifyValue:NO forCharacteristic:characteristic];
+        
+        [_centralManager cancelPeripheralConnection:peripheral];
+    }
+    
+    [_data appendData:characteristic.value];
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
+    
+    if (![characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID]]) {
+        return;
+    }
+    
+    if (characteristic.isNotifying) {
+        NSLog(@"Notification began on %@", characteristic);
+    } else {
+        // Notification has stopped
+        [_centralManager cancelPeripheralConnection:peripheral];
+    }
+}
+
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
+    _discoveredPeripheral = nil;
+    
+    [_centralManager scanForPeripheralsWithServices:@[[CBUUID UUIDWithString:TRANSFER_SERVICE_UUID]] options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
+}
+
+@end
